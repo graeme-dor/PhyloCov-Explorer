809fc470d9f6234c8f5b2aabdcddc65468e3aaea
Graeme14 <graemedor@gmail.com>
2026-03-24 10:12:18 +0200

    Add multi-page navigation architecture

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
+import { resolve } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default {
-  base: './'
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        datasets: resolve(__dirname, 'datasets.html'),
+        about: resolve(__dirname, 'about.html')
+      }
+    }
+  }
 }
